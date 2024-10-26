8dd94c188c05fd2a3ab180a421bd119a267b1e1b
Fahim1837 <fahimrb37@gmail.com>
2024-10-26 19:32:28 +0600

    1. Active Class Fixed 2. nvmrc file fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,26 @@
 'use client'
 import React from 'react'
-import { useRouter } from 'next/navigation'
 import LogoText from '@/assets/images/Logo_text_no_bg.png'
 import LogoIconGif from '@/assets/images/Logo-icon-gif.gif'
 import Image from 'next/image'
 import { FaBars } from 'react-icons/fa6'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 
 
 
 const Navbar = () => {
   const router = useRouter()
+  const pathName = usePathname()
+
+  const capitalizeFirstLetter = (string:string) => {
+    const cleanString = string.replace(/^\//, ''); 
+  return cleanString.charAt(0).toUpperCase() + cleanString.slice(1);
+  };
+
+  const activeClass = 'text-primary scale-110 '
+  const normalClass = 'hover:text-primary hover:scale-110'
+  const links = ['/services', '/approach', '/portfolio', '/about']
 
   return (
     <header className='border-b border-primary sticky top-0 z-50 bg-bg-1'>
@@ -23,10 +33,9 @@ const Navbar = () => {
             </Link>
             <FaBars className='h-7 w-7 lg:hidden' />
             <nav className='hidden lg:flex gap-10 items-center text-text-1/80 font-semibold'>
-              <Link href="/services" className='hover:text-primary'>Services</Link>
-              <Link href="/approach" className='hover:text-primary'>Approach</Link>
-              <Link href="/portfolio" className='hover:text-primary'>Portfolio</Link>
-              <Link href="/about" className='hover:text-primary'>About</Link>
+            {links.map ((item) => (
+              <Link key={item} href={item} className={item === pathName ? activeClass + normalClass : normalClass}>{capitalizeFirstLetter(item)}</Link>
+            )) }
               <button className='btn' onClick={()=>{router.push('/contact')}}>{`Let's Talk Business`}</button>
             </nav>
           </div>
